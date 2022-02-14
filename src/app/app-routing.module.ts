c51fc4bc99e8206d6d8e51440eb90dd058729e27
Aaron Cervantes <acaaron25@gmail.com>
2022-02-13 22:18:56 -0600

    Creacion del modulo auth
    Contendra las rutas de login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Err404Component } from './componentes/vistas/err404/err404.component';
-import { LoginComponent } from './componentes/vistas/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './componentes/vistas/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 import { Err404Component } from './componentes/vistas/err404/err404.component';
 import { AdministradorModule } from './componentes/vistas/adminUser/administrador.module';
 import { ClienteModule } from './componentes/vistas/clienteUser/cliente.module';
-import { LoginModule } from './componentes/vistas/login/login.module';
 import { UsuarioSistemaModule } from './componentes/vistas/sistemaUser/usuario-sistema.module';
 import { PublicModule } from './public/public.module';
 
@@ -26,7 +25,6 @@ import { PublicModule } from './public/public.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoginModule,
     AdministradorModule,
     ClienteModule,
     UsuarioSistemaModule,
